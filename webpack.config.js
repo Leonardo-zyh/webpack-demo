b039be06789dc4e251610792c0a7ed944598d992
Leonardo_zyh <956731563@qq.com>
2018-09-06 09:37:10 +0800

    config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist/')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "postcss-loader",
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    }
+    ]
+  }
+};
